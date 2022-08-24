01889948cbca6235f122d1bebbf4c7e4aea6967f
AbrahamRV <aramirez20235@ufide.ac.cr>
2022-08-24 00:19:38 -0600

    fincatica base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'http://localhost:3000/api',
-  herokuUrl: 'https://fincatica-backend.herokuapp.com/api',
+  // herokuUrl: 'https://fincatica-backend.herokuapp.com/api',
+  herokuUrl: 'https://dry-garden-25019.herokuapp.com/api',
 };
